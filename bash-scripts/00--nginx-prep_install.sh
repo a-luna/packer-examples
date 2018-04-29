520a354c717656cfe9ef6dac7980bbcb33f57854
Aaron Luna <aaronluna@gmail.com>
2018-04-28 21:38:32 -0700

    Update 00--nginx-prep_install.sh

--- bash-scripts/00--nginx-prep_install.sh
+++ bash-scripts/00--nginx-prep_install.sh
@@ -21,20 +21,25 @@ sudo touch $INSTALL_LOG_FILE_PATH
 sudo chown ubuntu:ubuntu $INSTALL_LOG_FILE_PATH
 
 echo "$(date +"%d-%b-%Y-%H-%M-%S") | Updating system..." |& tee -a ${INSTALL_LOG_FILE_PATH}
+
 # Add Maxmind PPA to apt sources
 sudo add-apt-repository ppa:maxmind/ppa -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
-# Update OS - SEE NOTE ABOVE IF INSTALLING ON UBUNTU 16.04 or 17.10 ON AMAZON EC2
-#sudo apt update >> ${INSTALL_LOG_FILE_PATH} 2>&1 && sudo apt upgrade -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
+
+# Update OS
 sudo apt update >> ${INSTALL_LOG_FILE_PATH} 2>&1 && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
 sudo apt autoremove -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
 
 echo "$(date +"%d-%b-%Y-%H-%M-%S") | Downloading prerequisites..." |& tee -a ${INSTALL_LOG_FILE_PATH}
+
 # Install build tools (gcc, g++, etc)
-sudo apt install build-essential -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
+sudo apt install build-essential -y >> ${INSTALL_LOG_FILE_PATH} 2>&
+
 # Install libraries required by GeoIP2 module to read MaxMind database files
 sudo apt install libmaxminddb0 libmaxminddb-dev mmdb-bin -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
+
 # Install checkinstall to create .deb package file
 sudo apt install checkinstall -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
+
 # Install Uncomplicated Firewall (UFW) since NGINX app profile 
 # is created after install and directory is assumed to exist
 sudo apt install ufw -y >> ${INSTALL_LOG_FILE_PATH} 2>&1
